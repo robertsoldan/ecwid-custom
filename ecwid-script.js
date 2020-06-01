e85a301558597631c2e3a8ffc93c4def077035ae
Robert Soldan <contact@robertsoldan.com>
2020-06-01 16:17:15 +0000

    Update ecwid-script.js

--- ecwid-script.js
+++ ecwid-script.js
@@ -1,29 +1,15 @@
-// The field "how_did_you_find_us" asks user about how they found the store. Drop down type
-ec.order.extraFields.how_did_you_find_us = {
-    'title': 'How did you find us?',
-    'type': 'select',
+// Initialize extra fields
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+
+// Add a new optional text input 'How should we sign the package?' to shipping address form
+ec.order.extraFields.wrapping_box_signature = {
+    'title': 'How should we sign the package?',
+    'textPlaceholder': 'Package sign',
+    'type': 'text',
+    'tip': 'We will put a label on a box so the recipient knows who it is from',
     'required': false,
-    'selectOptions': ['Google Ads', 'Friend told me', 'TV show', 'Other'],
-    'value': 'TV show', // Default value
-    'checkoutDisplaySection': 'payment_details'
-};
-
-Ecwid.refreshConfig();
-
-// Add pickup time selection for customer
-ec.order.extraFields.ecwid_pickup_time = {
-    'title': '_msg_ShippingDetails.pickup.customer_header',
-    'required': true,
-    'type': 'datetime',
-    'checkoutDisplaySection': 'pickup_details',
-    'orderDetailsDisplaySection': 'payment_details',
-}
-
-Ecwid.refreshConfig();
-
-// Hidden field, which is not shown at checkout
-ec.order.extraFields.my_custom_field = {
-    'value': 'abcd12345'
+    'checkoutDisplaySection': 'shipping_address'
 };
 
 Ecwid.refreshConfig();
