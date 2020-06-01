2e572ce5693afed21fb769d52411765ecdf1dd86
Robert Soldan <contact@robertsoldan.com>
2020-06-01 15:29:58 +0000

    Update ecwid-script.js

--- ecwid-script.js
+++ ecwid-script.js
@@ -2,14 +2,14 @@
 ec.order = ec.order || {};
 ec.order.extraFields = ec.order.extraFields || {};
 
-// Add a new optional text input 'How should we sign the package?' to shipping address form
+// Add a new optional text input 'How should we sign the package?' to local delivery address form
 ec.order.extraFields.wrapping_box_signature = {
     'title': 'How should we sign the package?',
     'textPlaceholder': 'Package sign',
     'type': 'text',
     'tip': 'We will put a label on a box so the recipient knows who it is from',
     'required': false,
-    'checkoutDisplaySection': 'shipping_address'
+    'checkoutDisplaySection': 'local_delivery'
 };
 
 Ecwid.refreshConfig();
