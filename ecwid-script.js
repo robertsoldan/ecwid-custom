d6b5f98a02ff12490ecdcf8ffe9fb2d98b603bc3
Robert Soldan <contact@robertsoldan.com>
2020-06-01 16:32:02 +0000

    Update ecwid-script.js

--- ecwid-script.js
+++ ecwid-script.js
@@ -46,45 +46,11 @@ ec.order.extraFields.my_custom_field = {
 Ecwid.refreshConfig();
 */
 
-/*
- * Show a promo 'Free shipping' message on the cart page. 
- * In the example below, users will see a message on the cart page 
- * informing them that they will qualify for free shipping if their 
- * order is more than $99
- */
+window.ec = window.ec || Object();
+window.ec.config = window.ec.config || Object();
+window.ec.config.navigation_scrolling = "CUSTOM";
 
-var promoMessage = "Orders $99 and up ship free!";
-var minSubtotal = 99;
-var widgets;
+window.ec.config.custom_scroller = function() {
+  window.scrollTo(500, 0);
+};
 
-// Calculating subtotal and displaying the message
-var checkSubtotal = function(order) {
-  if (order) {
-    var subtotal = order.total - order.shipping;
-    if (subtotal > minSubtotal) {
-      alert(promoMessage);
-    }  
-  }
-}
-
-// Detecting whether we're on the cart page and get the cart info
-Ecwid.OnPageLoaded.add(function(page) {
-  widgets = Ecwid.getInitializedWidgets();
-
-  // if storefront widget is present on this page
-  for (i = 0; i < widgets.length ; i++) {
-    if (widgets[i] == "ProductBrowser") {
-      if ('CART' == page.type) {
-        Ecwid.Cart.calculateTotal(function(order) {
-          checkSubtotal(order);
-        });
-      }
-    }
-  }
-});
-
-// Get color value for the message and store it in color variable
-var color = Ecwid.getAppPublicConfig(appId);
-
-// your code here
-// ...
